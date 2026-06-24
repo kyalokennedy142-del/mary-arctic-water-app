f325444f0133f5c2a1aea389babbaddb639e7b4d
Kennedy Kyallo <kyalokennedy142@gmail.com>
2026-06-24 01:53:51 -0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mary-arctic-water-app/sw.js', { scope: '/mary-arctic-water-app/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
